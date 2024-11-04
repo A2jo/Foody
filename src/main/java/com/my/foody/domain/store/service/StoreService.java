eea03d1dcd7303db11048c945f2447fd9c0c1c96
TIP_Santa <bhb89@naver.com>
2024-11-04 14:09:03 +0900

    See issue #9 feat(store-service) : 가게 수 검사 기능 추가

--- src/main/java/com/my/foody/domain/store/service/StoreService.java
+++ src/main/java/com/my/foody/domain/store/service/StoreService.java
@@ -2,12 +2,9 @@ package com.my.foody.domain.store.service;
 
 import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
 import com.my.foody.domain.store.dto.resp.StoreCreateRespDto;
-import com.my.foody.domain.store.entity.Store;
 import com.my.foody.domain.store.repo.StoreRepository;
-import com.my.foody.global.config.valid.RequireAuth;
 import com.my.foody.global.ex.BusinessException;
 import com.my.foody.global.ex.ErrorCode;
-import com.my.foody.global.jwt.UserType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +18,15 @@ public class StoreService {
     private final StoreRepository storeRepository;
 
 //    @RequireAuth(userType = UserType.OWNER)
-    public StoreCreateRespDto createStore(@Valid StoreCreateReqDto storeCreatereqDto) {
+    public StoreCreateRespDto createStore(@Valid StoreCreateReqDto storeCreatereqDto, Long ownerId) {
         // 가게이름 중복 검사
         if(storeRepository.existsByName(storeCreatereqDto.getName())){
             throw new BusinessException(ErrorCode.STORENAME_ALREADY_EXISTS);
         }
-        //
+        // 사장님 가게 3개 이상 생성 불가
+        if(storeRepository.countByOwnerId(ownerId) >= 3){
+            throw new BusinessException(ErrorCode.HAVE_FULL_STORE);
+        }
 
     }
 }
