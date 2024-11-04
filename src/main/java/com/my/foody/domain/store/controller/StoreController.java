628bafe8d716a9b5798ffe358eaa45d58eaf2d1d
TIP_Santa <bhb89@naver.com>
2024-11-04 13:15:53 +0900

    See issue #9 feat(store-controller) : 가게 생성 기능 추가

--- src/main/java/com/my/foody/domain/store/controller/StoreController.java
+++ src/main/java/com/my/foody/domain/store/controller/StoreController.java
@@ -1,4 +1,26 @@
 package com.my.foody.domain.store.controller;
 
+import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
+import com.my.foody.domain.store.service.StoreService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/owners")
+@RequiredArgsConstructor
 public class StoreController {
+
+    private final StoreService storeService;
+
+    @PostMapping("/stores")
+    public ResponseEntity<String> createStore(@RequestBody @Valid StoreCreateReqDto reqDto) {
+        storeService.createStore(reqDto);
+        return ResponseEntity.ok("가게 생성 성공");
+    }
+
 }
