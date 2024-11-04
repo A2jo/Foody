3ffa681f4d7959d857ecabab0fa0d1cd80d1c885
KIMHYEJIN <0411khj@naver.com>
2024-11-04 17:22:20 +0900

    feat(dto): OwnerJoinReqDto 생성 및 유효성 검사 추가 #14
    - OwnerJoinReqDto 필드 정의
    - 유효성 검사 어노테이션 추가

--- src/main/java/com/my/foody/domain/owner/dto/req/OwnerJoinReqDto.java
+++ src/main/java/com/my/foody/domain/owner/dto/req/OwnerJoinReqDto.java
@@ -1,4 +1,31 @@
 package com.my.foody.domain.owner.dto.req;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
 public class OwnerJoinReqDto {
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @Size(max = 15)
+    private String contact;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(max = 60)
+    private String password;
+
+    @NotNull
+    private Boolean isDeleted;
+
+
 }
