b9c0ee16e48ea56b56ab32c8485cb639b49eebe2
TIP_Santa <bhb89@naver.com>
2024-11-04 14:10:06 +0900

    See issue #9 feat(store-service) : OWNER 권한 확인 추가

--- src/main/java/com/my/foody/domain/store/service/StoreService.java
+++ src/main/java/com/my/foody/domain/store/service/StoreService.java
@@ -3,8 +3,10 @@ package com.my.foody.domain.store.service;
 import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
 import com.my.foody.domain.store.dto.resp.StoreCreateRespDto;
 import com.my.foody.domain.store.repo.StoreRepository;
+import com.my.foody.global.config.valid.RequireAuth;
 import com.my.foody.global.ex.BusinessException;
 import com.my.foody.global.ex.ErrorCode;
+import com.my.foody.global.jwt.UserType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +19,7 @@ public class StoreService {
 
     private final StoreRepository storeRepository;
 
-//    @RequireAuth(userType = UserType.OWNER)
+    @RequireAuth(userType = UserType.OWNER)
     public StoreCreateRespDto createStore(@Valid StoreCreateReqDto storeCreatereqDto, Long ownerId) {
         // 가게이름 중복 검사
         if(storeRepository.existsByName(storeCreatereqDto.getName())){
