2c03f396cc794d4d5caf52bd9a37743a93589f68
TIP_Santa <bhb89@naver.com>
2024-11-04 17:17:14 +0900

    See issue #9 chore(store-service) : 문서 포멧팅

--- src/main/java/com/my/foody/domain/store/service/StoreService.java
+++ src/main/java/com/my/foody/domain/store/service/StoreService.java
@@ -6,10 +6,8 @@ import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
 import com.my.foody.domain.store.dto.resp.StoreCreateRespDto;
 import com.my.foody.domain.store.entity.Store;
 import com.my.foody.domain.store.repo.StoreRepository;
-import com.my.foody.global.config.valid.RequireAuth;
 import com.my.foody.global.ex.BusinessException;
 import com.my.foody.global.ex.ErrorCode;
-import com.my.foody.global.jwt.UserType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,18 +21,17 @@ public class StoreService {
     private final StoreRepository storeRepository;
     private final OwnerRepository ownerRepository;
 
-    @RequireAuth(userType = UserType.OWNER)
     public StoreCreateRespDto createStore(@Valid StoreCreateReqDto storeCreatereqDto, Long ownerId) {
         // owner 조회
         Owner owner = ownerRepository.findById(ownerId).orElseThrow(() ->
                 new BusinessException(ErrorCode.OWNER_NOT_FOUND)
         );
         // 가게이름 중복 검사
-        if(storeRepository.existsByName(storeCreatereqDto.getName())){
+        if (storeRepository.existsByName(storeCreatereqDto.getName())) {
             throw new BusinessException(ErrorCode.STORENAME_ALREADY_EXISTS);
         }
         // 사장님 가게 3개 이상 생성 불가
-        if(storeRepository.countByOwnerId(ownerId) >= 3){
+        if (storeRepository.countByOwnerId(ownerId) >= 3) {
             throw new BusinessException(ErrorCode.HAVE_FULL_STORE);
         }
         // 가게 생성
@@ -46,9 +43,7 @@ public class StoreService {
                 .openTime(storeCreatereqDto.getOpenTime())
                 .endTime(storeCreatereqDto.getEndTime())
                 .build();
-
         storeRepository.save(store);
-
         return new StoreCreateRespDto(store);
     }
 }
