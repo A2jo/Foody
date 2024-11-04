0f0225c19809942dd6261c32aea1c0b4fdc976bc
TIP_Santa <bhb89@naver.com>
2024-11-04 13:48:43 +0900

    See issue #9 feat(store-create-req-dto) : Getter, Setter 어노테이션 추가

--- src/main/java/com/my/foody/domain/store/dto/req/StoreCreateReqDto.java
+++ src/main/java/com/my/foody/domain/store/dto/req/StoreCreateReqDto.java
@@ -1,7 +1,12 @@
 package com.my.foody.domain.store.dto.req;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalTime;
 
+@Getter
+@Setter
 public class StoreCreateReqDto {
     private String name;
     private String description;
