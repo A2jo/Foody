a7c8f830a1bf50fc75171f33d7b643b905081e0d
KIMHYEJIN <0411khj@naver.com>
2024-11-04 20:15:28 +0900

    feat(controller): OwnerController에 로그인 API 추가 #16
    - 사장님 로그인 요청을 처리할 POST /api/owners/login API 추가
    - 로그인 요청 처리 후 OwnerLoginRespDto 반환

--- src/main/java/com/my/foody/domain/owner/controller/OwnerController.java
+++ src/main/java/com/my/foody/domain/owner/controller/OwnerController.java
@@ -2,7 +2,9 @@ package com.my.foody.domain.owner.controller;
 
 
 import com.my.foody.domain.owner.dto.req.OwnerJoinReqDto;
+import com.my.foody.domain.owner.dto.req.OwnerLoginReqDto;
 import com.my.foody.domain.owner.dto.resp.OwnerJoinRespDto;
+import com.my.foody.domain.owner.dto.resp.OwnerLoginRespDto;
 import com.my.foody.domain.owner.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,6 +27,12 @@ public class OwnerController {
     @PostMapping("/signup")
     public ResponseEntity<OwnerJoinRespDto> signup(@RequestBody OwnerJoinReqDto ownerJoinReqDto) {
         OwnerJoinRespDto responseMessage = ownerService.signup(ownerJoinReqDto);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<OwnerLoginRespDto> login(@RequestBody OwnerLoginReqDto ownerLoginReqDto) {
+        OwnerLoginRespDto responseMessage = ownerService.login(ownerLoginReqDto);
         return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
 }
