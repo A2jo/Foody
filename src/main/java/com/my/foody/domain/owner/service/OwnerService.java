a8a5f591713a410649458d606d01c4a852c81aa8
KIMHYEJIN <0411khj@naver.com>
2024-11-04 17:44:15 +0900

    feat(service): OwnerService 회원가입 로직 및 비밀번호 암호화 추가 #14
    - 회원가입 시 이메일 중복 검사 및 비밀버호 암호화
    - 회원가입 성공 시 OwnerJoinRspDto 반환

--- src/main/java/com/my/foody/domain/owner/service/OwnerService.java
+++ src/main/java/com/my/foody/domain/owner/service/OwnerService.java
@@ -1,4 +1,49 @@
 package com.my.foody.domain.owner.service;
 
+import com.my.foody.domain.owner.dto.req.OwnerJoinReqDto;
+import com.my.foody.domain.owner.dto.resp.OwnerJoinRespDto;
+import com.my.foody.domain.owner.entity.Owner;
+import com.my.foody.domain.owner.repo.OwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private static final String SUCCESS_MESSAGE = "회원가입 완료 되었습니다.";
+
+
+    @Autowired
+    public OwnerService(OwnerRepository ownerRepository, PasswordEncoder passwordEncoder) {
+        this.ownerRepository = ownerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public OwnerJoinRespDto signup(OwnerJoinReqDto reqDto) {
+        validateEmailUniqueness(reqDto.getEmail());
+
+        Owner owner = Owner.builder()
+                .name(reqDto.getName())
+                .contact(reqDto.getContact())
+                .email(reqDto.getEmail())
+                .password(passwordEncoder.encode(reqDto.getPassword()))
+                .isDeleted(false)
+                .build();
+
+        ownerRepository.save(owner);
+
+        return new OwnerJoinRespDto(SUCCESS_MESSAGE);
+    }
+
+    // 이메일 중복 확인
+    private void validateEmailUniqueness(String email) {
+        if (ownerRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+    }
 }
