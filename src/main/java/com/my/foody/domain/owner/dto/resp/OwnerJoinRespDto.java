66bd1027ceb27f441b4240c92677fbc1d6ea2a82
KIMHYEJIN <0411khj@naver.com>
2024-11-04 17:25:23 +0900

    feat(dto): OwnerJoinRespDto 생성 #14
    - 회원가입 성공 시 성공 메시지를 반환

--- src/main/java/com/my/foody/domain/owner/dto/resp/OwnerJoinRespDto.java
+++ src/main/java/com/my/foody/domain/owner/dto/resp/OwnerJoinRespDto.java
@@ -1,4 +1,16 @@
 package com.my.foody.domain.owner.dto.resp;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class OwnerJoinRespDto {
+
+    private String message;
+    public OwnerJoinRespDto(String message) {
+        this.message = message;
+    }
+
 }
