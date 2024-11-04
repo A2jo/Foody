68335f975ae611f7fa0b0145fa9e1bee4b44753f
TIP_Santa <bhb89@naver.com>
2024-11-04 20:39:21 +0900

    feat(store-create-req-dto) : categoryIds 추가

--- src/main/java/com/my/foody/domain/store/dto/req/StoreCreateReqDto.java
+++ src/main/java/com/my/foody/domain/store/dto/req/StoreCreateReqDto.java
@@ -1,12 +1,15 @@
 package com.my.foody.domain.store.dto.req;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalTime;
+import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class StoreCreateReqDto {
     private String name;
     private String description;
@@ -15,4 +18,5 @@ public class StoreCreateReqDto {
     private LocalTime openTime;
     private LocalTime endTime;
     private boolean isDeleted;
+    private List<Long> categoryIds;
 }
