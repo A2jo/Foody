1b23dd50258f8565e9cd6d7a60912045bde4dc3b
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-04 11:27:24 +0900

    feat(user-controller): 회원가입 기능 추가 #3

--- src/main/java/com/my/foody/domain/user/controller/UserController.java
+++ src/main/java/com/my/foody/domain/user/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.my.foody.domain.user.controller;
 
+import com.my.foody.domain.user.dto.req.UserSignUpReqDto;
+import com.my.foody.domain.user.dto.resp.UserSignUpRespDto;
 import com.my.foody.domain.user.service.UserService;
 import com.my.foody.global.util.api.ApiResult;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-
-    public void register(){
-
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResult<UserSignUpRespDto>> signUp(@RequestBody @Valid UserSignUpReqDto userSignUpReqDto){
+        return new ResponseEntity<>(ApiResult.success(userService.signUp(userSignUpReqDto)), HttpStatus.CREATED);
     }
 
 
