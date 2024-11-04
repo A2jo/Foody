463b3850262aa2f3bd52500d4f69ab8d1be0b3c9
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-04 14:09:58 +0900

    feat(user-service): 유저 로그인 기능 추가 #8

--- src/main/java/com/my/foody/domain/user/service/UserService.java
+++ src/main/java/com/my/foody/domain/user/service/UserService.java
@@ -1,11 +1,18 @@
 package com.my.foody.domain.user.service;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.my.foody.domain.user.dto.req.UserLoginReqDto;
 import com.my.foody.domain.user.dto.req.UserSignUpReqDto;
+import com.my.foody.domain.user.dto.resp.UserLoginRespDto;
 import com.my.foody.domain.user.dto.resp.UserSignUpRespDto;
 import com.my.foody.domain.user.entity.User;
 import com.my.foody.domain.user.repo.UserRepository;
 import com.my.foody.global.ex.BusinessException;
 import com.my.foody.global.ex.ErrorCode;
+import com.my.foody.global.jwt.JwtProvider;
+import com.my.foody.global.jwt.TokenSubject;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
     public UserSignUpRespDto signUp(UserSignUpReqDto userSignUpReqDto){
         //이메일 중복 검사
@@ -31,5 +39,14 @@ public class UserService {
         userRepository.save(user);
         return new UserSignUpRespDto();
     }
+
+    public UserLoginRespDto login(UserLoginReqDto userLoginReqDto){
+        User user = userRepository.findByEmail(userLoginReqDto.getEmail())
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+        user.matchPassword(userLoginReqDto.getPassword());
+        String token = jwtProvider.create(TokenSubject.of(user));
+        return new UserLoginRespDto(token);
+    }
+
 
 }
