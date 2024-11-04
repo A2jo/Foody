125305afc74006fe17c22331a48b44edb314689d
soung90 <sounglikane@gmail.com>
2024-11-05 06:40:37 +0900

    feat(cart-dto): cart-create-req-dto 수정

--- src/main/java/com/my/foody/domain/cart/dto/req/CartCreateReqDto.java
+++ src/main/java/com/my/foody/domain/cart/dto/req/CartCreateReqDto.java
@@ -1,17 +1,5 @@
 package com.my.foody.domain.cart.dto.req;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@Getter
-@AllArgsConstructor
-@Builder
 public class CartCreateReqDto {
-    private int page; // current page number
-    private int limit; // page size
-    private long totalItems; // total number of items in the cart
-    private int totalPages;
 }
