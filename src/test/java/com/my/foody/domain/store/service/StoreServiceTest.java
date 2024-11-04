8d4882004fd70da6be462fece0aa0c7d510d09a0
TIP_Santa <bhb89@naver.com>
2024-11-04 16:44:36 +0900

    See issue #9 test(store-service) : 실패 테스트 - 동일한 가게이름이 존재하는 경우

--- src/test/java/com/my/foody/domain/store/service/StoreServiceTest.java
+++ src/test/java/com/my/foody/domain/store/service/StoreServiceTest.java
@@ -7,6 +7,9 @@ import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
 import com.my.foody.domain.store.dto.resp.StoreCreateRespDto;
 import com.my.foody.domain.store.entity.Store;
 import com.my.foody.domain.store.repo.StoreRepository;
+import com.my.foody.global.ex.BusinessException;
+import com.my.foody.global.ex.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,7 +19,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -31,6 +34,7 @@ public class StoreServiceTest {
     @Mock
     OwnerRepository ownerRepository;
 
+    // 가게 생성 성공 테스트
     @Test
     public void testCreateStore_Success() {
         StoreCreateReqDto storeCreateReqDto = new StoreCreateReqDto();
@@ -53,4 +57,28 @@ public class StoreServiceTest {
         assertNotNull(storeCreateRespDto);
         verify(storeRepository).save(any(Store.class));
     }
+
+    // 가게 생성 실패 테스트 - 동일한 가게 이름
+    @DisplayName("실패 테스트 - 동일한 이름")
+    @Test
+    public void testCreateStore_Fail_DuplicateStoreName() {
+        StoreCreateReqDto storeCreateReqDto = new StoreCreateReqDto();
+        Long ownerId = 1L;
+        storeCreateReqDto.setName("Test Store");
+
+        Owner owner = mock(Owner.class);
+
+        when(ownerRepository.findById(ownerId)).thenReturn(Optional.of(owner));
+        when(storeRepository.existsByName(storeCreateReqDto.getName())).thenReturn(true);
+
+        BusinessException exception = assertThrows(BusinessException.class, () ->
+                storeService.createStore(storeCreateReqDto, ownerId)
+        );
+
+        assertEquals(ErrorCode.STORENAME_ALREADY_EXISTS, exception.getErrorCode());
+        System.out.println(exception.getMessage());
+    }
+
+    // 가게 생성 실패 테스트 - 가게 생성 수 제한
+
 }
