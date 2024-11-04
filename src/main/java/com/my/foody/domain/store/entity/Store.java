51bd26093ea5901eb48f27448cb8ddc116841845
TIP_Santa <bhb89@naver.com>
2024-11-04 14:49:17 +0900

    See issue #9 feat(store) : builder 어노테이션 추가

--- src/main/java/com/my/foody/domain/store/entity/Store.java
+++ src/main/java/com/my/foody/domain/store/entity/Store.java
@@ -3,14 +3,13 @@ package com.my.foody.domain.store.entity;
 import com.my.foody.domain.base.BaseEntity;
 import com.my.foody.domain.owner.entity.Owner;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store extends BaseEntity {
     @Id
@@ -41,4 +40,17 @@ public class Store extends BaseEntity {
 
     @Column(nullable = false)
     private Boolean isDeleted; //삭제되면 true, 운영 중이면 false
+
+    @Builder
+    public Store(String name, Owner owner, String description, String contact, Long minOrderAmount, LocalTime openTime, LocalTime endTime){
+        this.name = name;
+        this.owner = owner;
+        this.description = description;
+        this.contact = contact;
+        this.minOrderAmount = minOrderAmount;
+        this.openTime = openTime;
+        this.endTime = endTime;
+        this.isDeleted = false;
+    }
+
 }
