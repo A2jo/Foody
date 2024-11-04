96ccd3f3ec82f2d2f0d5821d3a561964e41e998f
TIP_Santa <bhb89@naver.com>
2024-11-04 14:52:55 +0900

    See issue #9 feat(store-service) : 가게 생성 기능 추가

--- src/main/java/com/my/foody/domain/store/service/StoreService.java
+++ src/main/java/com/my/foody/domain/store/service/StoreService.java
@@ -1,7 +1,10 @@
 package com.my.foody.domain.store.service;
 
+import com.my.foody.domain.owner.entity.Owner;
+import com.my.foody.domain.owner.repo.OwnerRepository;
 import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
 import com.my.foody.domain.store.dto.resp.StoreCreateRespDto;
+import com.my.foody.domain.store.entity.Store;
 import com.my.foody.domain.store.repo.StoreRepository;
 import com.my.foody.global.config.valid.RequireAuth;
 import com.my.foody.global.ex.BusinessException;
@@ -18,9 +21,14 @@ import org.springframework.stereotype.Service;
 public class StoreService {
 
     private final StoreRepository storeRepository;
+    private final OwnerRepository ownerRepository;
 
     @RequireAuth(userType = UserType.OWNER)
     public StoreCreateRespDto createStore(@Valid StoreCreateReqDto storeCreatereqDto, Long ownerId) {
+        // owner 조회
+        Owner owner = ownerRepository.findById(ownerId).orElseThrow(() ->
+                new BusinessException(ErrorCode.OWNER_NOT_FOUND)
+        );
         // 가게이름 중복 검사
         if(storeRepository.existsByName(storeCreatereqDto.getName())){
             throw new BusinessException(ErrorCode.STORENAME_ALREADY_EXISTS);
@@ -29,6 +37,18 @@ public class StoreService {
         if(storeRepository.countByOwnerId(ownerId) >= 3){
             throw new BusinessException(ErrorCode.HAVE_FULL_STORE);
         }
+        // 가게 생성
+        Store store = Store.builder().name(storeCreatereqDto.getName())
+                .owner(owner)
+                .description(storeCreatereqDto.getDescription())
+                .contact(storeCreatereqDto.getContact())
+                .minOrderAmount(storeCreatereqDto.getMinOrderAmount())
+                .openTime(storeCreatereqDto.getOpenTime())
+                .endTime(storeCreatereqDto.getEndTime())
+                .build();
 
+        storeRepository.save(store);
+
+        return new StoreCreateRespDto(store);
     }
 }
