7d7183255c05b05f5a25b63d6e604f5137fbe5df
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-04 15:40:01 +0900

    feat(user-service): 마이페이지 조회 기능 추가 #13

--- src/main/java/com/my/foody/domain/user/dto/resp/UserInfoRespDto.java
+++ src/main/java/com/my/foody/domain/user/dto/resp/UserInfoRespDto.java
@@ -1,2 +1,21 @@
-package com.my.foody.domain.user.dto.resp;public class UserInfoRespDto {
+package com.my.foody.domain.user.dto.resp;
+
+import com.my.foody.domain.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserInfoRespDto {
+    private String name;
+    private String nickname;
+    private String contact;
+    private String email;
+
+    public UserInfoRespDto(User user) {
+        this.name = user.getName();
+        this.nickname = user.getNickname();
+        this.contact = user.getContact();
+        this.email = user.getEmail();
+    }
 }
