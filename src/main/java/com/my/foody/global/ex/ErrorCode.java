9d13ddf0b91c7eafcf0248ca61bfd22231427ea7
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-04 20:31:14 +0900

    feat(user-service): 주소지 수정 시 주소지 작성자 검증 로직 추가 #15

--- src/main/java/com/my/foody/global/ex/ErrorCode.java
+++ src/main/java/com/my/foody/global/ex/ErrorCode.java
@@ -18,6 +18,8 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST.value(), "비밀번호가 일치하지 않습니다"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 인증 정보 입니다"),
     ADDRESS_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 주소지입니다"),
+    UNAUTHORIZED_ADDRESS_ACCESS(HttpStatus.UNAUTHORIZED.value(), "해당 주소지에 접근 권한이 없습니다"),
+    INVALID_ADDRESS_FORMAT(HttpStatus.BAD_REQUEST.value(), "유효하지 않은 주소 형식입니다"),
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "존재하지 않는 이메일입니다"),
     STORENAME_ALREADY_EXISTS(HttpStatus.CONFLICT.value(), "이미 사용 중인 가게이름입니다");
     ;
