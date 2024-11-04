c0c0ea506cbebddf752f9e5979edb2bf9c8aaaa6
soung90 <sounglikane@gmail.com>
2024-11-05 05:57:23 +0900

    feat(cart-dto): cart-create-resp-dto 추가

--- src/main/java/com/my/foody/domain/cart/dto/resp/CartCreateRespDto.java
+++ src/main/java/com/my/foody/domain/cart/dto/resp/CartCreateRespDto.java
@@ -1,4 +1,17 @@
 package com.my.foody.domain.cart.dto.resp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Builder
 public class CartCreateRespDto {
+    private int page;
+    private int limit;
+    private long totalItems;
+    private int totalPages;
 }
