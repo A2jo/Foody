4ed65c37388b583cd3ff6e2497501c9c42882af8
soung90 <sounglikane@gmail.com>
2024-11-05 05:57:08 +0900

    feat(cart-dto): cart-create-req-dto 추가

--- src/main/java/com/my/foody/domain/cart/dto/req/CartCreateReqDto.java
+++ src/main/java/com/my/foody/domain/cart/dto/req/CartCreateReqDto.java
@@ -1,4 +1,21 @@
 package com.my.foody.domain.cart.dto.req;
 
+import com.my.foody.domain.cart.dto.resp.CartCreateRespDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Builder
 public class CartCreateReqDto {
+    private String storeName;
+    private String menuName;
+    private Long menuPrice;
+    private Long quantity;
+    private Long totalAmount;
+    private Long minOrderAmount;
+    private CartCreateRespDto cartCreateRespDto;
 }
