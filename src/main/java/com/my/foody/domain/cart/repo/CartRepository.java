71e026bdbfa81a77ab5b3694f3fd4680eaabcb47
soung90 <sounglikane@gmail.com>
2024-11-05 06:17:41 +0900

    feat(cart-repo): cart-repository 추가

--- src/main/java/com/my/foody/domain/cart/repo/CartRepository.java
+++ src/main/java/com/my/foody/domain/cart/repo/CartRepository.java
@@ -1,9 +1,15 @@
 package com.my.foody.domain.cart.repo;
 
 import com.my.foody.domain.cart.entity.Cart;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.id = :userId")
+    Page<Cart> findByUserId(@Param("userId") Long userId, Pageable pageable);
 }
