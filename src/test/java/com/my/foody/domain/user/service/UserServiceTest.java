d551c8decf74943412eb0bde2502d3a08bfc969f
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-04 11:54:48 +0900

    test(user-service): 회원가입 실패 테스트 추가 #3

--- src/test/java/com/my/foody/domain/user/service/UserServiceTest.java
+++ src/test/java/com/my/foody/domain/user/service/UserServiceTest.java
@@ -4,6 +4,8 @@ import com.my.foody.domain.user.dto.req.UserSignUpReqDto;
 import com.my.foody.domain.user.dto.resp.UserSignUpRespDto;
 import com.my.foody.domain.user.entity.User;
 import com.my.foody.domain.user.repo.UserRepository;
+import com.my.foody.global.ex.BusinessException;
+import com.my.foody.global.ex.ErrorCode;
 import com.my.foody.global.util.PasswordEncoder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,7 +29,7 @@ class UserServiceTest {
 
     @DisplayName(value = "회원가입 성공 테스트")
     @Test
-    void 회원가입_성공_테스트() throws Exception{
+    void signup_Success() throws Exception{
         //given
         UserSignUpReqDto signUpReqDto = UserSignUpReqDto.builder()
                 .contact("010-1234-5678")
@@ -47,6 +50,29 @@ class UserServiceTest {
         verify(userRepository, times(1)).existsByEmail(signUpReqDto.getEmail());
         verify(userRepository, times(1)).existsByNickname(signUpReqDto.getNickname());
         verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName(value = "회원가입 실패 테스트: 이메일 중복")
+    void signup_EmailAlreadyExists(){
+        //given
+        UserSignUpReqDto signUpReqDto = UserSignUpReqDto.builder()
+                .contact("010-1234-5678")
+                .email("user1234@naver.com")
+                .password(PasswordEncoder.encode("Password1234!"))
+                .name("userA")
+                .nickname("userrrr")
+                .build();
+
+        when(userRepository.existsByEmail(signUpReqDto.getEmail())).thenReturn(true);
+
+        //then
+        assertThatThrownBy(() -> userService.signUp(signUpReqDto))
+                          .isInstanceOf(BusinessException.class)
+                          .hasFieldOrPropertyWithValue("errorCode", ErrorCode.EMAIL_ALREADY_EXISTS);
+        verify(userRepository, times(1)).existsByEmail(signUpReqDto.getEmail());
+        verify(userRepository, never()).existsByNickname(signUpReqDto.getNickname());
+        verify(userRepository, never()).save(any(User.class));
     }
 
 }
