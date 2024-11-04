9b2e4e57a9abd13f7ab1404d804e8fc503f7597e
TIP_Santa <bhb89@naver.com>
2024-11-04 14:52:22 +0900

    See issue #9 feat(store-create-resp-dto) : storeCreateRespDto 구성 추가

--- src/main/java/com/my/foody/domain/store/dto/resp/StoreCreateRespDto.java
+++ src/main/java/com/my/foody/domain/store/dto/resp/StoreCreateRespDto.java
@@ -1,4 +1,23 @@
 package com.my.foody.domain.store.dto.resp;
 
+import com.my.foody.domain.store.entity.Store;
+
+import java.time.LocalTime;
+
 public class StoreCreateRespDto {
+    private String name;
+    private String Description;
+    private String contact;
+    private Long MinOrderAmount;
+    private LocalTime openTime;
+    private LocalTime endTime;
+
+    public StoreCreateRespDto(Store store) {
+        this.name = store.getName();
+        this.Description = store.getDescription();
+        this.contact = store.getContact();
+        this.MinOrderAmount = store.getMinOrderAmount();
+        this.openTime = store.getOpenTime();
+        this.endTime = store.getEndTime();
+    }
 }
