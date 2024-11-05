25b2ed80311daf753aa3a76dd67bc773c08f1c58
JIMINJI1 <mj013031@gmail.com>
2024-11-06 08:46:50 +0900

    feat(menu-repository): 메뉴 조회 repository(해당 가게 메뉴 전체 조회) #66

--- src/main/java/com/my/foody/domain/menu/repo/MenuRepository.java
+++ src/main/java/com/my/foody/domain/menu/repo/MenuRepository.java
@@ -1,9 +1,12 @@
 package com.my.foody.domain.menu.repo;
 
 import com.my.foody.domain.menu.entity.Menu;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Page<Menu> findAllByStoreId(Long storeId, Pageable pageable);
 }
