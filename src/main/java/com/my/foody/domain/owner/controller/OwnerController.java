3a4e9c2820bb8965741bb55f3a45bcc7950bc98b
KIMHYEJIN <0411khj@naver.com>
2024-11-05 13:46:28 +0900

    feat(controller): 사장님 마이페이지 수정 API 추가 및 기존 기능 개선 #27
    - 사장님 마이페이지 수정 API (/mypage) 추가
      - @PatchMapping으로 기존 정보 수정 가능
      - 수정된 정보는 OwnerMyPageUpdateRespDto로 반환

--- src/main/java/com/my/foody/domain/owner/controller/OwnerController.java
+++ src/main/java/com/my/foody/domain/owner/controller/OwnerController.java
@@ -3,9 +3,11 @@ package com.my.foody.domain.owner.controller;
 
 import com.my.foody.domain.owner.dto.req.OwnerJoinReqDto;
 import com.my.foody.domain.owner.dto.req.OwnerLoginReqDto;
+import com.my.foody.domain.owner.dto.req.OwnerMyPageUpdateReqDto;
 import com.my.foody.domain.owner.dto.resp.OwnerJoinRespDto;
 import com.my.foody.domain.owner.dto.resp.OwnerLoginRespDto;
 import com.my.foody.domain.owner.dto.resp.OwnerMyPageRespDto;
+import com.my.foody.domain.owner.dto.resp.OwnerMyPageUpdateRespDto;
 import com.my.foody.domain.owner.service.OwnerService;
 import com.my.foody.global.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +36,17 @@ public class OwnerController {
     @GetMapping("/mypage")
     public ResponseEntity<ApiResult<OwnerMyPageRespDto>> getMyPage(@AuthenticationPrincipal Long ownerId) {
         OwnerMyPageRespDto response = ownerService.getMyPage(ownerId);
+        return ResponseEntity.ok(ApiResult.success(response));
+    }
+
+    // 마이페이지 수정
+    @PatchMapping("/mypage")
+    public ResponseEntity<ApiResult<OwnerMyPageUpdateRespDto>> updateMyPage(
+            @AuthenticationPrincipal Long ownerId,
+            @RequestBody OwnerMyPageUpdateReqDto updateReqDto) {
+
+        // 서비스에서 업데이트 처리 후 응답 DTO 반환
+        OwnerMyPageUpdateRespDto response = ownerService.updateMyPage(ownerId, updateReqDto);
         return ResponseEntity.ok(ApiResult.success(response));
     }
 }
