7964f69439dda2d9cc613dab9f371c33d3c3755d
KIMHYEJIN <0411khj@naver.com>
2024-11-05 17:55:57 +0900

    refactor: 비밀번호 유효성 검증 로직 통일화
    - OwnerJoinReqDto, OwnerLoginReqDto, OwnerMyPageUpdateReqDto의 비밀번호 유효성 검증을 통일하여 일관성 유지

--- src/main/java/com/my/foody/domain/owner/dto/req/OwnerJoinReqDto.java
+++ src/main/java/com/my/foody/domain/owner/dto/req/OwnerJoinReqDto.java
@@ -1,9 +1,6 @@
 package com.my.foody.domain.owner.dto.req;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
@@ -22,6 +19,8 @@ public class OwnerJoinReqDto {
 
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
     @Size(min = 8, max = 60, message = "비밀번호는 8자 이상, 60자 이하로 설정해야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "는 최소 8자, 하나 이상의 문자와 숫자를 포함해야 합니다.")
     private String password;
 
     @NotNull(message = "삭제 여부는 필수 입력 항목입니다.")
