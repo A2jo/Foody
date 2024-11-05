f6db2305be7e67bebefa838291778d250f1c2598
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-05 11:44:02 +0900

    feat(user-service): 유저 정보 수정 기능 추가 #35

--- src/main/java/com/my/foody/domain/user/dto/req/UserInfoModifyReqDto.java
+++ src/main/java/com/my/foody/domain/user/dto/req/UserInfoModifyReqDto.java
@@ -13,19 +13,15 @@ import org.hibernate.validator.constraints.Length;
 public class UserInfoModifyReqDto {
 
     @Email(message = "유효하지 않은 이메일 형식입니다")
-    @NotBlank(message = "이메일을 입력해야 합니다")
     private String email;
 
-    @NotBlank(message = "이름을 입력해야 합니다")
     @Length(min = 1, max = 100, message = "이름은 최소 1자 최대 100자로 입력해야 합니다")
     private String name;
 
-    @NotBlank(message = "전화번호를 입력해야 합니다")
     @Length(max = 15, message = "전화번호는 15자 이내여야 합니다")
     @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다")
     private String contact;
 
-    @NotBlank(message = "닉네임을 입력해야 합니다")
     @Length(min = 1, max = 100, message = "닉네임은 최소 1자 최대 100자로 입력해야 합니다")
     private String nickname;
 }
