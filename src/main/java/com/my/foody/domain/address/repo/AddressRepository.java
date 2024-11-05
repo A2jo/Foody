49f5e1dac20e08b6b7e4b1ae9451e0f009b06582
soung90 <sounglikane@gmail.com>
2024-11-05 18:19:11 +0900

    feat(address-repo): method 추가 #41

--- src/main/java/com/my/foody/domain/address/repo/AddressRepository.java
+++ src/main/java/com/my/foody/domain/address/repo/AddressRepository.java
@@ -1,9 +1,11 @@
 package com.my.foody.domain.address.repo;
 
 import com.my.foody.domain.address.entity.Address;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    Optional<Address> findByUserId(Long userId);
 }
