f3df18bc1319e6de982499d77d0eab7532b70370
soung90 <sounglikane@gmail.com>
2024-11-05 19:55:05 +0900

    refactor(address-repo): method 수정 #41

--- src/main/java/com/my/foody/domain/address/repo/AddressRepository.java
+++ src/main/java/com/my/foody/domain/address/repo/AddressRepository.java
@@ -1,11 +1,16 @@
 package com.my.foody.domain.address.repo;
 
 import com.my.foody.domain.address.entity.Address;
+
+import java.util.List;
 import java.util.Optional;
+
+import com.my.foody.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
     Optional<Address> findByUserId(Long userId);
+    List<Address> findAllByUserOrderByCreatedAtDesc(User user);
 }
