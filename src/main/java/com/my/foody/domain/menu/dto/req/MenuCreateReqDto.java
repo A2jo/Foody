6aea40b97c6fe495195ba2c4e7091fdc996d1143
JIMINJI1 <mj013031@gmail.com>
2024-11-06 00:52:13 +0900

    feat(menu-dto): @NoArgsConstructor 생성자 추가 #11

--- src/main/java/com/my/foody/domain/menu/dto/req/MenuCreateReqDto.java
+++ src/main/java/com/my/foody/domain/menu/dto/req/MenuCreateReqDto.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class MenuCreateReqDto {
 
     @Size(min = 1, max = 30, message = "메뉴 이름은 1자 이상 30자 이하여야 합니다")
