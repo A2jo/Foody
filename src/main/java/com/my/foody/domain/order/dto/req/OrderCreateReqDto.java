a5c69a22b1f3b42bc3bebc942dfc6b03a6b3f82e
soung90 <sounglikane@gmail.com>
2024-11-06 00:38:43 +0900

    feat(order-dto): order-create-req-dto 추가 #10

--- src/main/java/com/my/foody/domain/order/dto/req/OrderCreateReqDto.java
+++ src/main/java/com/my/foody/domain/order/dto/req/OrderCreateReqDto.java
@@ -1,4 +1,15 @@
 package com.my.foody.domain.order.dto.req;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderCreateReqDto {
+    private Long addressId;
+    private Long userId;
+    private Long totalAmount;
+    private Long menuId;
 }
