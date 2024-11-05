07f9fc2e9dc992b59517b4f555735dd5e8a2bbd5
JIMINJI1 <mj013031@gmail.com>
2024-11-05 20:31:24 +0900

    Revert "feat(menu-dto): 메뉴 수정 dto  #48"
    This reverts commit 060ed822715db27841a92a9044bced1720a3991e.

--- src/main/java/com/my/foody/domain/menu/dto/resp/MenuCreateRespDto.java
+++ src/main/java/com/my/foody/domain/menu/dto/resp/MenuCreateRespDto.java
@@ -1,20 +1,4 @@
 package com.my.foody.domain.menu.dto.resp;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public class MenuCreateRespDto {
-    private static final String SUCCESS_MESSAGE = "메뉴 등록 되었습니다";
-    private final String message;
-
-    public MenuCreateRespDto() {
-        this.message = SUCCESS_MESSAGE;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
 }
