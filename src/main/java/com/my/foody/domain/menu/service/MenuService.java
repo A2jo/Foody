b1620313225a7e94bbdd7db1a576cc548808bd97
JIMINJI1 <mj013031@gmail.com>
2024-11-05 13:39:32 +0900

    feat(menu-service): 메뉴 등록 service 추가 #11

--- src/main/java/com/my/foody/domain/menu/service/MenuService.java
+++ src/main/java/com/my/foody/domain/menu/service/MenuService.java
@@ -1,4 +1,45 @@
 package com.my.foody.domain.menu.service;
 
+import com.my.foody.domain.menu.dto.req.MenuCreateReqDto;
+import com.my.foody.domain.menu.dto.resp.MenuCreateRespDto;
+import com.my.foody.domain.menu.entity.Menu;
+import com.my.foody.domain.menu.repo.MenuRepository;
+import com.my.foody.domain.store.entity.Store;
+import com.my.foody.domain.store.repo.StoreRepository;
+import com.my.foody.global.ex.BusinessException;
+import com.my.foody.global.ex.ErrorCode;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class MenuService {
+
+    private MenuRepository menuRepository;
+    private StoreRepository storeRepository;
+
+    // 메뉴 등록
+    public MenuCreateRespDto createMenu(Long storeId, MenuCreateReqDto menuCreateReqDto, Long ownerId) {
+
+        Store store = storeRepository.findById(storeId).orElseThrow(() -> new BusinessException(ErrorCode.STORE_NOT_FOUND));
+
+        //주인인지아닌지 확인
+        if (!store.getOwner().getId().equals(ownerId)) {
+            throw new BusinessException(ErrorCode.FORBIDDEN_ACCESS);
+        }
+
+        Menu menu = Menu.builder()
+                .store(store)
+                .name(menuCreateReqDto.getName())
+                .price(menuCreateReqDto.getPrice())
+                .isSoldOut(false)
+                .isDeleted(false)
+                .build();
+
+        menuRepository.save(menu);
+
+        // 메세제 응답 반환
+        return new MenuCreateRespDto();
+    }
+
 }
