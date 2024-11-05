95e00df1317751451ebcfda892bd420b7a506a21
soung90 <sounglikane@gmail.com>
2024-11-05 23:34:10 +0900

    refactor(address-repo): method 추가 #28

--- src/main/java/com/my/foody/domain/address/repo/AddressRepository.java
+++ src/main/java/com/my/foody/domain/address/repo/AddressRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    Optional<Address> findByUserId(Long userId);
     List<Address> findAllByUserOrderByCreatedAtDesc(User user);
+    Optional<Address> findByUserIdAndIsMain(Long userId, Boolean isMain);
 }
