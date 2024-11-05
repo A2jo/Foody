1d96ffa582c7292098fd11f645894048c11b430a
KIMHYEJIN <0411khj@naver.com>
2024-11-05 17:10:17 +0900

    feat: 사장님 회원 탈퇴 요청 DTO 추가 #39
    - OwnerDeleteReqDto 생성하여 회원 탈퇴 시 비밀번호 검증 기능 추가
    - 비밀번호 형식 검증: 최소 8자, 60자 이하, 하나 이상의 문자와 숫자 포함

---
+++ src/main/java/com/my/foody/domain/owner/dto/req/OwnerDeleteReqDto.java
@@ -0,0 +1,16 @@
+package com.my.foody.domain.owner.dto.req;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class OwnerDeleteReqDto {
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    @Size(min = 8, max = 60, message = "비밀번호는 8자 이상, 60자 이하로 설정해야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "비밀번호는 최소 8자, 하나 이상의 문자와 숫자를 포함해야 합니다.")
+    private String password;
+}
