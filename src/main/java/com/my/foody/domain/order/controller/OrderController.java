8f10c7a9ad2ded18240f4978ee1cc02f622e1b62
soung90 <sounglikane@gmail.com>
2024-11-05 18:42:41 +0900

    feat(order-controller): 주문 완료 전 페이지 기능 추가 #41

--- src/main/java/com/my/foody/domain/order/controller/OrderController.java
+++ src/main/java/com/my/foody/domain/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.my.foody.domain.order.controller;
 
 import com.my.foody.domain.order.dto.req.OrderStatusUpdateReqDto;
+import com.my.foody.domain.order.dto.resp.OrderPreviewRespDto;
 import com.my.foody.domain.order.dto.resp.OrderStatusUpdateRespDto;
 import com.my.foody.domain.order.service.OrderService;
+import com.my.foody.domain.user.service.UserService;
 import com.my.foody.global.config.valid.CurrentUser;
 import com.my.foody.global.config.valid.RequireAuth;
 import com.my.foody.global.jwt.TokenSubject;
@@ -11,28 +13,42 @@ import com.my.foody.global.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
-    private final OrderService orderService;
 
-    @PutMapping("api/owners/orders/{orderId}")
-    @RequireAuth(userType = UserType.OWNER)
-    public ResponseEntity<ApiResult<OrderStatusUpdateRespDto>> changeOrderStatus(@PathVariable("orderId") Long orderId,
-                                                                                 @RequestBody OrderStatusUpdateReqDto requestDto,
-                                                                                 @CurrentUser TokenSubject tokenSubject) {
-        // ownerId
-        Long ownerId = tokenSubject.getId();
+  private final OrderService orderService;
+  private final UserService userService;
 
-        // 주문 상태 업데이트 및 응답 DTO 반환
-        OrderStatusUpdateRespDto responseDto = orderService.updateOrderStatus(requestDto, orderId, ownerId);
-        ApiResult<OrderStatusUpdateRespDto> apiResult = ApiResult.success(responseDto);
+  @PutMapping("api/owners/orders/{orderId}")
+  @RequireAuth(userType = UserType.OWNER)
+  public ResponseEntity<ApiResult<OrderStatusUpdateRespDto>> changeOrderStatus(
+      @PathVariable("orderId") Long orderId,
+      @RequestBody OrderStatusUpdateReqDto requestDto,
+      @CurrentUser TokenSubject tokenSubject) {
 
-        return new ResponseEntity<>(apiResult, HttpStatus.OK);
-    }
+    Long ownerId = tokenSubject.getId();
+
+    // 주문 상태 업데이트 및 응답 DTO 반환
+    OrderStatusUpdateRespDto responseDto = orderService.updateOrderStatus(requestDto, orderId, ownerId);
+    ApiResult<OrderStatusUpdateRespDto> apiResult = ApiResult.success(responseDto);
+
+    return new ResponseEntity<>(apiResult, HttpStatus.OK);
+  }
+
+  @GetMapping("/api/home/stores/{storeId}/cart/{cartId}/orders")
+  @RequireAuth(userType = UserType.USER)
+  public ResponseEntity<ApiResult<OrderPreviewRespDto>> getOrderPreview(
+          @PathVariable Long storeId,
+          @PathVariable Long cartId,
+          @CurrentUser TokenSubject tokenSubject) {
+
+    Long userId = tokenSubject.getId();
+    OrderPreviewRespDto orderPreview = orderService.getOrderPreview(userId, storeId, cartId);
+    ApiResult<OrderPreviewRespDto> apiResult = ApiResult.success(orderPreview);
+
+    return new ResponseEntity<>(apiResult, HttpStatus.OK);
+  }
 }
