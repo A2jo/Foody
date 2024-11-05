f3c6359a553da5f0b22c3a7f1928f4cb7d92f949
soung90 <sounglikane@gmail.com>
2024-11-05 19:40:40 +0900

    refactor(main class): 공백 지우기 #41

--- src/main/java/com/my/foody/FoodyApplication.java
+++ src/main/java/com/my/foody/FoodyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FoodyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FoodyApplication.class, args);
     }
-
 }
