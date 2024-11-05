16d388b3077cc4a0571ee6c390fb6a7227fcf70c
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-06 00:20:23 +0900

    feat(cart-menu): CartMenu 엔티티 생성 #60

--- src/main/java/com/my/foody/domain/cart/entity/Cart.java
+++ src/main/java/com/my/foody/domain/cart/entity/Cart.java
@@ -18,16 +18,11 @@ public class Cart extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "menu_id")
-    private Menu menu;
-    private Long quantity;
 }
