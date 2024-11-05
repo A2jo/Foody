3ae488f4159c9885d0587dcca1604ed5da0f343b
JIMINJI1 <mj013031@gmail.com>
2024-11-06 00:32:20 +0900

    feat(menu-controller): 메뉴삭제 controller 추가  #49

--- src/main/java/com/my/foody/domain/menu/controller/MenuController.java
+++ src/main/java/com/my/foody/domain/menu/controller/MenuController.java
@@ -1,5 +1,21 @@
 package com.my.foody.domain.menu.controller;
 
+import com.my.foody.domain.menu.dto.resp.MenuDeleteRespDto;
+import com.my.foody.domain.menu.service.MenuService;
+import com.my.foody.global.config.valid.CurrentUser;
+import com.my.foody.global.config.valid.RequireAuth;
+import com.my.foody.global.jwt.TokenSubject;
+import com.my.foody.global.jwt.UserType;
+import com.my.foody.global.util.api.ApiResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 import com.my.foody.domain.menu.dto.req.MenuUpdateReqDto;
 import com.my.foody.domain.menu.dto.resp.MenuUpdateRespDto;
 import com.my.foody.domain.menu.service.MenuService;
@@ -39,6 +55,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 @RequiredArgsConstructor
 public class MenuController {
+
+    private final MenuService menuService;
+
+    // 메뉴삭제
+    @PatchMapping("/api/owners/stores/{storeId}/menus/{menuId}")
+    //권한 확인
+    @RequireAuth(userType = UserType.OWNER)
+    public ResponseEntity<ApiResult<MenuDeleteRespDto>> softDeleteMenu(@PathVariable("storeId") Long storeId, @PathVariable("menuId") Long menuId,
+                                                                       @CurrentUser TokenSubject tokenSubject) {
+        //ownerId
+        Long ownerId = tokenSubject.getId();
+        MenuDeleteRespDto responseDto = menuService.SoftDeleteMenu(storeId, menuId, ownerId);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(responseDto));
+    }
 
     private final MenuService menuService;
 
