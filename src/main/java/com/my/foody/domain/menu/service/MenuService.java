9293a66704af11cbf155a4c4d2f5ae1cc5ad4e39
JIMINJI1 <mj013031@gmail.com>
2024-11-06 08:47:53 +0900

    feat(menu-service): 메뉴 조회 service #66

--- src/main/java/com/my/foody/domain/menu/service/MenuService.java
+++ src/main/java/com/my/foody/domain/menu/service/MenuService.java
@@ -1,4 +1,45 @@
 package com.my.foody.domain.menu.service;
 
+import com.my.foody.domain.menu.dto.resp.MenuReadResponseDto;
+import com.my.foody.domain.menu.entity.Menu;
+import com.my.foody.domain.menu.repo.MenuRepository;
+import com.my.foody.domain.store.entity.Store;
+import com.my.foody.domain.store.repo.StoreRepository;
+import com.my.foody.global.ex.BusinessException;
+import com.my.foody.global.ex.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MenuService {
+
+    private final MenuRepository menuRepository;
+    private final StoreRepository storeRepository;
+
+    //메뉴 전체 조회 (페이징처리)
+    public Page<MenuReadResponseDto> getMenus(Long storeId, Long ownerId, Pageable pageable) {
+        //해당 가게가 존재 하는지 확인
+        Store store = storeRepository.findById(storeId).orElseThrow(() -> new BusinessException(ErrorCode.STORE_NOT_FOUND));
+
+        //가게 주인 확인
+        isStoreOwner(store, ownerId);
+
+        //가게에 해당하는 모든 메뉴 조회
+        Page<Menu> menuPage = menuRepository.findAllByStoreId(storeId,pageable);
+
+        return menuPage.map(MenuReadResponseDto::new);
+    }
+
+
+
+    // 가게 주인 확인 메서드
+    private void isStoreOwner(Store store, Long ownerId) {
+        if (!store.getOwner().getId().equals(ownerId)) {
+            throw new BusinessException(ErrorCode.FORBIDDEN_ACCESS);
+        }
+    }
+
 }
