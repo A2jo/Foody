d45a321db05a8eae79f8e0925a21ea79ec4eda00
TIP_Santa <bhb89@naver.com>
2024-11-05 11:49:42 +0900

    feat(storeController) : 가게 조회 기능 구현 #30

--- src/main/java/com/my/foody/domain/store/controller/StoreController.java
+++ src/main/java/com/my/foody/domain/store/controller/StoreController.java
@@ -1,7 +1,6 @@
 package com.my.foody.domain.store.controller;
 
 import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
-import com.my.foody.domain.store.dto.resp.GetStoreRespDto;
 import com.my.foody.domain.store.dto.resp.StoreCreateRespDto;
 import com.my.foody.domain.store.service.StoreService;
 import com.my.foody.global.config.valid.CurrentUser;
@@ -29,10 +28,10 @@ public class StoreController {
         return new ResponseEntity<>(ApiResult.success(storeService.createStore(storeCreatereqDto, ownerId)), HttpStatus.CREATED);
     }
 
-//    @RequireAuth(userType = UserType.OWNER)
-//    @GetMapping("stores")
-//    public ResponseEntity<ApiResult<GetStoreRespDto>> getAllStoreByOwnerId(TokenSubject tokenSubject) {
-//        Long ownerId = tokenSubject.getId();
-//        return new ResponseEntity<>(ApiResult.success(storeService.getAllStoreByOwnerId(ownerId)), HttpStatus.CREATED);
-//    }
+    @RequireAuth(userType = UserType.OWNER)
+    @GetMapping("stores")
+    public ResponseEntity<ApiResult<GetStoreRespDto>> getAllStoreByOwnerId(TokenSubject tokenSubject) {
+        Long ownerId = tokenSubject.getId();
+        return new ResponseEntity<>(ApiResult.success(storeService.getAllStoreByOwnerId(ownerId)), HttpStatus.CREATED);
+    }
 }
