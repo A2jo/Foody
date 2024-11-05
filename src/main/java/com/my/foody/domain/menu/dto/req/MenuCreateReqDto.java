7151a2155c0e62faaf0f23382f52f068e3e265dc
JIMINJI1 <mj013031@gmail.com>
2024-11-05 20:31:24 +0900

    Revert "feat(menu-dto): 메뉴 수정 dto  #48"
    This reverts commit 060ed822715db27841a92a9044bced1720a3991e.

--- src/main/java/com/my/foody/domain/menu/dto/req/MenuCreateReqDto.java
+++ src/main/java/com/my/foody/domain/menu/dto/req/MenuCreateReqDto.java
@@ -10,18 +10,4 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MenuCreateReqDto {
-
-    @Size(min = 1, max = 30, message = "메뉴 이름은 1자 이상 30자 이하여야 합니다")
-    @NotBlank(message = "메뉴 이름을 입력해야 합니다")
-    private String name;
-
-    @NotNull(message = "메뉴 가격을 입력해야 합니다")
-    @Min(value = 1, message = "메뉴 가격은 1원 이상이어야 합니다")
-    private Long price;
-
-    public MenuCreateReqDto(String name, Long price) {
-        this.name = name;
-        this.price = price;
-    }
-
 }
