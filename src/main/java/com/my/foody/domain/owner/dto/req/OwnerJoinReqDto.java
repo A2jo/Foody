46c0e5afff29d1eccc107994743fca30f14d85ce
KIMHYEJIN <0411khj@naver.com>
2024-11-05 12:46:08 +0900

    refactor(dto): OwnerJoinReqDto 유효성 검사 메시지 추가 #14
    - 이름, 연락처, 이메일, 비밀번호 필드에 유효성 검사 메시지 추가
    - 비밀번호는 최소 8자 이상, 최대 60자로 제한
    - isDeleted 필드에 필수 입력 메시지 추가

--- src/main/java/com/my/foody/domain/owner/dto/req/OwnerJoinReqDto.java
+++ src/main/java/com/my/foody/domain/owner/dto/req/OwnerJoinReqDto.java
@@ -9,23 +9,21 @@ import lombok.Getter;
 @Getter
 public class OwnerJoinReqDto {
 
-    @NotBlank
-    @Size(max = 100)
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
+    @Size(max = 100, message = "이름은 최대 100자까지 가능합니다.")
     private String name;
 
-    @Size(max = 15)
+    @Size(max = 15, message = "전화번호는 최대 15자까지 가능합니다.")
     private String contact;
 
-    @Email
-    @NotBlank
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
     private String email;
 
-    @NotBlank
-    @Size(max = 60)
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    @Size(min = 8, max = 60, message = "비밀번호는 8자 이상, 60자 이하로 설정해야 합니다.")
     private String password;
 
-    @NotNull
+    @NotNull(message = "삭제 여부는 필수 입력 항목입니다.")
     private Boolean isDeleted;
-
-
 }
