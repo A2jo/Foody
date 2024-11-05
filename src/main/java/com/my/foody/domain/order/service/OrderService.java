8b2ceec042be4110302d72d2eb6c968cc27bad8e
soung90 <sounglikane@gmail.com>
2024-11-05 18:44:52 +0900

    feat(order-service): method 추가 #41

--- src/main/java/com/my/foody/domain/order/service/OrderService.java
+++ src/main/java/com/my/foody/domain/order/service/OrderService.java
@@ -1,9 +1,19 @@
 package com.my.foody.domain.order.service;
 
+import com.my.foody.domain.address.entity.Address;
+import com.my.foody.domain.address.repo.AddressRepository;
+import com.my.foody.domain.cart.entity.Cart;
+import com.my.foody.domain.cart.repo.CartRepository;
+import com.my.foody.domain.menu.entity.Menu;
 import com.my.foody.domain.order.dto.req.OrderStatusUpdateReqDto;
+import com.my.foody.domain.order.dto.resp.OrderPreviewRespDto;
 import com.my.foody.domain.order.dto.resp.OrderStatusUpdateRespDto;
 import com.my.foody.domain.order.entity.Order;
 import com.my.foody.domain.order.repo.OrderRepository;
+import com.my.foody.domain.store.entity.Store;
+import com.my.foody.domain.store.repo.StoreRepository;
+import com.my.foody.domain.user.entity.User;
+import com.my.foody.domain.user.repo.UserRepository;
 import com.my.foody.global.ex.BusinessException;
 import com.my.foody.global.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +25,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+    private UserRepository userRepository;
+    private final CartRepository cartRepository;
+    private final StoreRepository storeRepository;
+    private final AddressRepository addressRepository;
+
 
     public OrderStatusUpdateRespDto updateOrderStatus(OrderStatusUpdateReqDto requestDto, Long orderId, Long ownerId) {
         Order order = orderRepository.findById(orderId).orElseThrow(() -> new BusinessException(ErrorCode.ORDER_NOT_FOUND));
@@ -30,5 +45,34 @@ public class OrderService {
 
         // 응답 DTO 생성
         return new OrderStatusUpdateRespDto(savedOrder.getOrderStatus().name());
+    }
+
+    public OrderPreviewRespDto getOrderPreview(Long userId, Long storeId, Long cartId) {
+        // Fetch the user
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        Address address = addressRepository.findByUserId(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ADDRESS_NOT_FOUND));
+
+        Cart cart = cartRepository.findByIdAndUserIdAndStoreId(cartId, userId, storeId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.CART_ITEM_NOT_FOUND));
+
+        Store store = cart.getStore();
+        Menu menu = cart.getMenu();
+
+        Long totalAmount = menu.getPrice() * cart.getQuantity();
+
+        return OrderPreviewRespDto.builder()
+                .roadAddress(address.getRoadAddress())
+                .detailedAddress(address.getDetailedAddress())
+                .userContact(user.getContact())
+                .storeName(store.getName())
+                .storeId(store.getId())
+                .menuName(menu.getName())
+                .menuPrice(menu.getPrice())
+                .quantity(cart.getQuantity())
+                .totalAmount(totalAmount)
+                .build();
     }
 }
