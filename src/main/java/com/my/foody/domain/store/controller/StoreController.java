1d5193a5042660f7d23b8c562faef7c1882fb5ca
TIP_Santa <bhb89@naver.com>
2024-11-05 10:38:14 +0900

    refactor(store-controller) : TokenSubject 앞에 @CurrentUser 추가 #9

--- src/main/java/com/my/foody/domain/store/controller/StoreController.java
+++ src/main/java/com/my/foody/domain/store/controller/StoreController.java
@@ -1,8 +1,10 @@
 package com.my.foody.domain.store.controller;
 
 import com.my.foody.domain.store.dto.req.StoreCreateReqDto;
+import com.my.foody.domain.store.dto.resp.GetStoreRespDto;
 import com.my.foody.domain.store.dto.resp.StoreCreateRespDto;
 import com.my.foody.domain.store.service.StoreService;
+import com.my.foody.global.config.valid.CurrentUser;
 import com.my.foody.global.config.valid.RequireAuth;
 import com.my.foody.global.jwt.TokenSubject;
 import com.my.foody.global.jwt.UserType;
@@ -11,10 +13,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/owners")
@@ -25,8 +24,15 @@ public class StoreController {
 
     @RequireAuth(userType = UserType.OWNER)
     @PostMapping("/stores")
-    public ResponseEntity<ApiResult<StoreCreateRespDto>> createStore(@RequestBody @Valid StoreCreateReqDto storeCreatereqDto, TokenSubject tokenSubject) {
+    public ResponseEntity<ApiResult<StoreCreateRespDto>> createStore(@RequestBody @Valid StoreCreateReqDto storeCreatereqDto,@CurrentUser TokenSubject tokenSubject) {
         Long ownerId = tokenSubject.getId();
         return new ResponseEntity<>(ApiResult.success(storeService.createStore(storeCreatereqDto, ownerId)), HttpStatus.CREATED);
     }
+
+//    @RequireAuth(userType = UserType.OWNER)
+//    @GetMapping("stores")
+//    public ResponseEntity<ApiResult<GetStoreRespDto>> getAllStoreByOwnerId(TokenSubject tokenSubject) {
+//        Long ownerId = tokenSubject.getId();
+//        return new ResponseEntity<>(ApiResult.success(storeService.getAllStoreByOwnerId(ownerId)), HttpStatus.CREATED);
+//    }
 }
