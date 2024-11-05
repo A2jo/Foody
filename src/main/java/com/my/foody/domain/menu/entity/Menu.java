8bbf0c0307bb6ab2285d15422b99a8010be40008
JIMINJI1 <mj013031@gmail.com>
2024-11-05 13:37:39 +0900

    feat(menu-entity): entity builder 추가 #11

--- src/main/java/com/my/foody/domain/menu/entity/Menu.java
+++ src/main/java/com/my/foody/domain/menu/entity/Menu.java
@@ -4,13 +4,14 @@ import com.my.foody.domain.base.BaseEntity;
 import com.my.foody.domain.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.jmx.export.annotation.ManagedNotifications;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Menu extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +32,14 @@ public class Menu extends BaseEntity {
 
     @Column(nullable = false)
     private Boolean isDeleted;
+
+    @Builder
+    public Menu(Store store, String name, Long price, Boolean isSoldOut, Boolean isDeleted) {
+        this.store = store;
+        this.name = name;
+        this.price = price;
+        this.isSoldOut = false;
+        this.isDeleted = false;
+    }
+
 }
