d0469f903194d7f0ec78fd20777f21388d14f7a9
ㅇi쀼z1 <yaewon0411@naver.com>
2024-11-05 09:45:55 +0900

    test(user-controller): 전체 주소지 조회 테스트 추가 #25

--- src/test/java/com/my/foody/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/my/foody/domain/user/controller/UserControllerTest.java
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.my.foody.domain.address.dto.req.AddressCreateReqDto;
 import com.my.foody.domain.address.dto.resp.AddressCreateRespDto;
 import com.my.foody.domain.address.entity.Address;
+import com.my.foody.domain.user.dto.resp.AddressListRespDto;
 import com.my.foody.domain.user.dto.resp.UserInfoRespDto;
 import com.my.foody.domain.user.entity.User;
 import com.my.foody.domain.user.service.UserService;
+import com.my.foody.global.ex.BusinessException;
 import com.my.foody.global.jwt.JwtProvider;
 import com.my.foody.global.jwt.JwtVo;
 import com.my.foody.global.jwt.TokenSubject;
 import com.my.foody.global.jwt.UserType;
 import com.my.foody.global.util.DummyObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -123,5 +130,58 @@ public class UserControllerTest extends DummyObject {
                 .andDo(print());
 
         verify(userService, never()).registerAddress(any(), any());
+    }
+
+    @Test
+    @DisplayName("전체 주소지 조회 성공 테스트: 주소지 크기 5")
+    void getAllAddress_Success() throws Exception {
+        Long userId = 1L;
+        String token = "test.token";
+        TokenSubject tokenSubject = new TokenSubject(userId, UserType.USER);
+        User user = mockUser();
+
+        List<Address> addressList = new ArrayList<>();
+        for(int i = 0;i<5;i++){
+            addressList.add(mockAddress(user));
+        }
+
+        AddressListRespDto result = new AddressListRespDto(addressList);
+        
+        when(jwtProvider.validate(token)).thenReturn(tokenSubject);
+        when(userService.getAllAddress(userId)).thenReturn(result);
+
+        //when & then
+        mvc.perform(get("/api/users/mypage/address")
+                        .header(JwtVo.HEADER, JwtVo.TOKEN_PREFIX + token))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.addressList").isArray())
+                .andExpect(jsonPath("$.data.addressList", hasSize(addressList.size())))
+                .andExpect(jsonPath("$.data.addressList[0].roadAddress").exists())
+                .andExpect(jsonPath("$.data.addressList[0].detailedAddress").exists())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("전체 주소지 조회 성공 테스트: 빈 주소 목록")
+    void getAllAddress_EmptyList() throws Exception {
+        Long userId = 1L;
+        String token = "test.token";
+        TokenSubject tokenSubject = new TokenSubject(userId, UserType.USER);
+
+        AddressListRespDto result = new AddressListRespDto(new ArrayList<>());
+
+        when(jwtProvider.validate(token)).thenReturn(tokenSubject);
+        when(userService.getAllAddress(userId)).thenReturn(result);
+
+        // when & then
+        mvc.perform(get("/api/users/mypage/address")
+                        .header(JwtVo.HEADER, JwtVo.TOKEN_PREFIX + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.addressList").isArray())
+                .andExpect(jsonPath("$.data.addressList", hasSize(0)))
+                .andDo(print());
     }
 }
