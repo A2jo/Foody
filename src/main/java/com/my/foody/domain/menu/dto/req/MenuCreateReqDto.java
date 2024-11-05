9e39b13b3ddd8438e76caf047c8d5029d032f060
JIMINJI1 <mj013031@gmail.com>
2024-11-05 13:38:54 +0900

    feat(menu-dto): 메뉴 등록 dto 추가 #11

--- src/main/java/com/my/foody/domain/menu/dto/req/MenuCreateReqDto.java
+++ src/main/java/com/my/foody/domain/menu/dto/req/MenuCreateReqDto.java
@@ -1,4 +1,17 @@
 package com.my.foody.domain.menu.dto.req;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
 public class MenuCreateReqDto {
+
+    @Size(min = 1, max = 30, message = "메뉴 이름은 1자 이상 30자 이하여야 합니다")
+    @NotBlank(message = "메뉴 이름을 입력해야 합니다")
+    private String name;
+
+    @NotNull(message = "메뉴 가격을 입력해야 합니다")
+    private Long price;
 }
