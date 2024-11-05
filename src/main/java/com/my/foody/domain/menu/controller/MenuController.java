40cbb1477937167bcf2c9caa9ef8e2324e052b11
JIMINJI1 <mj013031@gmail.com>
2024-11-05 20:28:40 +0900

    feat(menu-controller): 메뉴 수정 controller  #48

--- src/main/java/com/my/foody/domain/menu/controller/MenuController.java
+++ src/main/java/com/my/foody/domain/menu/controller/MenuController.java
@@ -1,6 +1,28 @@
 package com.my.foody.domain.menu.controller;
 
 import com.my.foody.domain.menu.dto.req.MenuCreateReqDto;
+import com.my.foody.domain.menu.dto.req.MenuUpdateReqDto;
+import com.my.foody.domain.menu.dto.resp.MenuCreateRespDto;
+import com.my.foody.domain.menu.dto.resp.MenuUpdateRespDto;
+import com.my.foody.domain.menu.service.MenuService;
+import com.my.foody.global.config.valid.CurrentUser;
+import com.my.foody.global.config.valid.RequireAuth;
+import com.my.foody.global.jwt.TokenSubject;
+import com.my.foody.global.jwt.UserType;
+import com.my.foody.global.util.api.ApiResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequiredArgsConstructor
+import com.my.foody.domain.menu.dto.req.MenuCreateReqDto;
 import com.my.foody.domain.menu.dto.resp.MenuCreateRespDto;
 import com.my.foody.domain.menu.service.MenuService;
 import com.my.foody.global.config.valid.CurrentUser;
@@ -20,6 +42,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Controller
 @RequiredArgsConstructor
 public class MenuController {
+
+    private MenuService menuService;
+
+    // 2. 메뉴수정
+    @PutMapping("/api/owners/stores/{storeId}/menus/{menuId}")
+    //권한 확인
+    @RequireAuth(userType = UserType.OWNER)
+    public ResponseEntity<ApiResult<MenuUpdateRespDto>> updateMenu(@PathVariable("storeId") Long storeId, @PathVariable("menuId") Long menuId,
+                                                                   @Valid @RequestBody MenuUpdateReqDto menuUpdateReqDto,
+                                                                   @CurrentUser TokenSubject tokenSubject) {
+        //userId
+        Long ownerId = tokenSubject.getId();
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResult.success(menuService.updateMenu(storeId, menuId, menuUpdateReqDto, ownerId)));
+
+    }
 
     private final MenuService menuService;
 
