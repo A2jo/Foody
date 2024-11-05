1aa8615947f56704f00621cffb656a73df1711d2
TIP_Santa <bhb89@naver.com>
2024-11-06 02:37:13 +0900

    #64 test(StoreServiceTest) 성공 테스트 - 가게 조회 성공 테스트   - 가게 목록 정상 반환   - 빈 목록 반환

--- src/test/java/com/my/foody/domain/store/service/StoreServiceTest.java
+++ src/test/java/com/my/foody/domain/store/service/StoreServiceTest.java
@@ -48,7 +48,6 @@ public class StoreServiceTest {
     @Mock
     private StoreCategoryRepository storeCategoryRepository;
 
-    // -[가게 생성 테스트]---------------------------------------------------
     @DisplayName("가게 생성 성공")
     @Test
     public void testCreateStore_Success() {
@@ -464,4 +463,69 @@ public class StoreServiceTest {
         assertEquals(ErrorCode.NO_UPDATE_DATA, exception.getErrorCode());
         System.out.println(exception.getMessage());
     }
+    // -[카테고리 별 가게 조회]---------------------------------------------------
+    @DisplayName("가게 조회 성공 테스트 - 가게 목록 반환")
+    @Test
+    public void testGetStoreByCategory_Success() {
+        Long categoryId = 1L;
+
+        when(categoryRepository.existsById(categoryId)).thenReturn(true);
+
+        Store store1 = Store.builder()
+                .id(1L)
+                .name("Store A")
+                .minOrderAmount(10000L)
+                .isDeleted(false)
+                .openTime(LocalTime.of(9, 0))
+                .endTime(LocalTime.of(22, 0))
+                .build();
+
+        Store store2 = Store.builder()
+                .id(2L)
+                .name("Store B")
+                .minOrderAmount(15000L)
+                .isDeleted(false)
+                .openTime(LocalTime.of(10, 0))
+                .endTime(LocalTime.of(23, 0))
+                .build();
+
+        StoreCategory storeCategory1 = StoreCategory.builder()
+                .store(store1)
+                .build();
+
+        StoreCategory storeCategory2 = StoreCategory.builder()
+                .store(store2)
+                .build();
+
+        when(storeCategoryRepository.findByCategoryId(categoryId)).thenReturn(List.of(storeCategory1, storeCategory2));
+
+        List<GetStoreRespDto> result = storeService.getStoreByCategory(categoryId);
+
+        assertEquals(2, result.size());
+        assertEquals("Store A", result.get(0).getName());
+        assertEquals(10000L, result.get(0).getMinOrderAmount());
+        assertEquals("Store B", result.get(1).getName());
+        assertEquals(15000L, result.get(1).getMinOrderAmount());
+
+        verify(categoryRepository, times(1)).existsById(categoryId);
+        verify(storeCategoryRepository, times(1)).findByCategoryId(categoryId);
+    }
+
+    @DisplayName("가게 조회 성공 테스트 - 빈 목록 반환")
+    @Test
+    public void testGetStoreByCategory_Fail_NotFoundStore() {
+        Long categoryId = 1L;
+
+        when(categoryRepository.existsById(categoryId)).thenReturn(true);
+
+        when(storeCategoryRepository.findByCategoryId(categoryId)).thenReturn(List.of());
+
+        List<GetStoreRespDto> result = storeService.getStoreByCategory(categoryId);
+
+        assertEquals(0, result.size());
+
+        verify(categoryRepository, times(1)).existsById(categoryId); // 카테고리 존재 여부 확인 호출 검증
+        verify(storeCategoryRepository, times(1)).findByCategoryId(categoryId);
+    }
+
 }
