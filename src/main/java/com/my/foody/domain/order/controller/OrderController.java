0d75a27677391f73c8abda1eda922b62d9f8f520
soung90 <sounglikane@gmail.com>
2024-11-05 14:50:07 +0900

    feat(order-controller): 주문상태변경 API 추가 #41

--- src/main/java/com/my/foody/domain/order/controller/OrderController.java
+++ src/main/java/com/my/foody/domain/order/controller/OrderController.java
@@ -1,4 +1,38 @@
 package com.my.foody.domain.order.controller;
 
+import com.my.foody.domain.order.dto.req.OrderStatusUpdateReqDto;
+import com.my.foody.domain.order.dto.resp.OrderStatusUpdateRespDto;
+import com.my.foody.domain.order.service.OrderService;
+import com.my.foody.global.config.valid.CurrentUser;
+import com.my.foody.global.config.valid.RequireAuth;
+import com.my.foody.global.jwt.TokenSubject;
+import com.my.foody.global.jwt.UserType;
+import com.my.foody.global.util.api.ApiResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+
+    @PutMapping("api/owners/orders/{orderId}")
+    @RequireAuth(userType = UserType.OWNER)
+    public ResponseEntity<ApiResult<OrderStatusUpdateRespDto>> changeOrderStatus(@PathVariable("orderId") Long orderId,
+                                                                                 @RequestBody OrderStatusUpdateReqDto requestDto,
+                                                                                 @CurrentUser TokenSubject tokenSubject) {
+        // ownerId
+        Long ownerId = tokenSubject.getId();
+
+        // 주문 상태 업데이트 및 응답 DTO 반환
+        OrderStatusUpdateRespDto responseDto = orderService.updateOrderStatus(requestDto, orderId, ownerId);
+        ApiResult<OrderStatusUpdateRespDto> apiResult = ApiResult.success(responseDto);
+
+        return new ResponseEntity<>(apiResult, HttpStatus.OK);
+    }
 }
