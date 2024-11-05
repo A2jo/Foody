078e5404043b123267a2086c9953d72e0e3c04e9
JIMINJI1 <mj013031@gmail.com>
2024-11-05 11:28:25 +0900

    feat(global-util): mock객체 추가 #5

--- src/main/java/com/my/foody/global/util/DummyObject.java
+++ src/main/java/com/my/foody/global/util/DummyObject.java
@@ -1,7 +1,13 @@
 package com.my.foody.global.util;
 
 import com.my.foody.domain.address.entity.Address;
+import com.my.foody.domain.order.entity.Order;
+import com.my.foody.domain.owner.entity.OrderStatus;
+import com.my.foody.domain.owner.entity.Owner;
+import com.my.foody.domain.store.entity.Store;
 import com.my.foody.domain.user.entity.User;
+
+import java.time.LocalTime;
 
 public class DummyObject {
 
@@ -16,7 +22,6 @@ public class DummyObject {
                 .build();
     }
 
-
     protected Address mockAddress(User user){
         return Address.builder()
                 .user(user)
@@ -24,4 +29,70 @@ public class DummyObject {
                 .detailedAddress("상세주소")
                 .build();
     }
+    
+    //사장
+    protected Owner mockOwner(Long id){
+        String password = "password123!";
+        return Owner.builder()
+                .id(id)
+                .email("ownertest@naver.com")
+                .password(PasswordEncoder.encode(password))
+                .contact("010-1234-5678")
+                .name("ownerA")
+                .build();
+    }
+
+
+    //id 가진 user
+    protected User newUser(Long id){
+        String password = "Maeda1234!";
+        return User.builder()
+                .id(id)
+                .email("user1234@naver.com")
+                .password(PasswordEncoder.encode(password))
+                .contact("010-1234-5678")
+                .name("userA")
+                .nickname("userrr")
+                .build();
+    }
+
+
+    //사장
+    protected Owner newOwner(Long id){
+        String password = "password123!";
+        return Owner.builder()
+                .id(id)
+                .email("ownertest@naver.com")
+                .password(PasswordEncoder.encode(password))
+                .contact("010-1234-5678")
+                .name("ownerA")
+                .build();
+    }
+
+    //가게
+    protected Store mockStore(Owner owner) {
+        return Store.builder()
+                .name("가게이름")
+                .owner(owner)
+                .description("가게설명")
+                .contact("010-1234-5678")
+                .minOrderAmount(10000L)
+                .openTime(LocalTime.of(9, 0))
+                .endTime(LocalTime.of(22, 0))
+                .isDeleted(false)
+                .build();
+    }
+
+    //주문
+    protected Order mockOrder(Owner owner, User user, Address address) {
+        Store store = mockStore(owner);
+        return Order.builder()
+                .orderStatus(OrderStatus.PENDING)
+                .user(user)
+                .store(store)
+                .address(address)
+                .totalAmount(10000L)
+                .build();
+    }
+
 }
