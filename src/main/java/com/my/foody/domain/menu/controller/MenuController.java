47c1deea7775ca6d8cd0aae8211c26c7ea1e7fd0
JIMINJI1 <mj013031@gmail.com>
2024-11-06 15:01:41 +0900

    refactor(menu-controller): pageable에서 page,limit으로 변경 #66

--- src/main/java/com/my/foody/domain/menu/controller/MenuController.java
+++ src/main/java/com/my/foody/domain/menu/controller/MenuController.java
@@ -1,6 +1,5 @@
 package com.my.foody.domain.menu.controller;
 
-
 import com.my.foody.domain.menu.dto.resp.MenuReadResponseDto;
 import com.my.foody.domain.menu.service.MenuService;
 import com.my.foody.global.config.valid.CurrentUser;
@@ -10,10 +9,10 @@ import com.my.foody.global.jwt.UserType;
 import com.my.foody.global.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,11 +27,13 @@ public class MenuController {
     @RequireAuth(userType = UserType.OWNER)
     public ResponseEntity<ApiResult<Page<MenuReadResponseDto>>> getMenus(@PathVariable("storeId") Long storeId,
                                                                          @CurrentUser TokenSubject tokenSubject,
-                                                                         Pageable pageable) {
+                                                                         @RequestParam(defaultValue = "0") int page,
+                                                                         @RequestParam(defaultValue = "10") int limit) {
         //ownerId
         Long ownerId = tokenSubject.getId();
 
-        Page<MenuReadResponseDto> responseDto = menuService.getMenus(storeId, ownerId, pageable);
+
+        Page<MenuReadResponseDto> responseDto = menuService.getMenus(storeId, ownerId, page, limit);
         return ResponseEntity.ok(ApiResult.success(responseDto));
     }
 }
