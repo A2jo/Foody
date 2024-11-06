843c0fe069cbb0925c35883d5a44f20e1b71259f
TIP_Santa <bhb89@naver.com>
2024-11-06 09:57:24 +0900

    #64 feat(StoreController, StoreService) - StoreController : Page 기능 추가 - StoreService : Page 기능 추가 refactor(StoreCategoryRepository) - 반환 타입 List > Page 변경

--- src/main/java/com/my/foody/domain/storeCategory/repo/StoreCategoryRepository.java
+++ src/main/java/com/my/foody/domain/storeCategory/repo/StoreCategoryRepository.java
@@ -1,6 +1,8 @@
 package com.my.foody.domain.storeCategory.repo;
 
 import com.my.foody.domain.storeCategory.entity.StoreCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import java.util.List;
 
 @Repository
 public interface StoreCategoryRepository extends JpaRepository<StoreCategory, Long> {
+    Page<StoreCategory> findByCategoryId(Long categoryId, Pageable pageable);
     void deleteByStoreId(Long storeId);
     List<StoreCategory> findByCategoryId(Long categoryId);
 }
