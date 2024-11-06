57f8c68a3d63762d840f7d3992bf6c90ddc635a9
JIMINJI1 <mj013031@gmail.com>
2024-11-06 15:03:45 +0900

    refactor(menu-service): pageable 부분 수정 #66

--- src/main/java/com/my/foody/domain/menu/service/MenuService.java
+++ src/main/java/com/my/foody/domain/menu/service/MenuService.java
@@ -38,6 +38,7 @@ import com.my.foody.global.ex.BusinessException;
 import com.my.foody.global.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -49,19 +50,21 @@ public class MenuService {
     private final StoreRepository storeRepository;
 
     //메뉴 전체 조회 (페이징처리)
-    public Page<MenuReadResponseDto> getMenus(Long storeId, Long ownerId, Pageable pageable) {
+    public Page<MenuReadResponseDto> getMenus(Long storeId, Long ownerId, int page, int limit) {
         //해당 가게가 존재 하는지 확인
         Store store = storeRepository.findById(storeId).orElseThrow(() -> new BusinessException(ErrorCode.STORE_NOT_FOUND));
 
         //가게 주인 확인
         isStoreOwner(store, ownerId);
 
+        //pageable 객체 생성
+        Pageable pageable = PageRequest.of(page, limit);
+
         //가게에 해당하는 모든 메뉴 조회
-        Page<Menu> menuPage = menuRepository.findAllByStoreId(storeId,pageable);
+        Page<Menu> menuPage = menuRepository.findAllByStoreId(storeId, pageable);
 
         return menuPage.map(MenuReadResponseDto::new);
     }
-
 
 
     // 가게 주인 확인 메서드
