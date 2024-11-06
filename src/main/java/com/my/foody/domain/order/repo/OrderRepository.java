88570d581a08e9670cb336e04122462e0337e701
soung90 <sounglikane@gmail.com>
2024-11-07 04:04:46 +0900

    feat(order-repo): query 문 추가 #80

--- src/main/java/com/my/foody/domain/order/repo/OrderRepository.java
+++ src/main/java/com/my/foody/domain/order/repo/OrderRepository.java
@@ -1,13 +1,14 @@
 package com.my.foody.domain.order.repo;
 
 import com.my.foody.domain.order.entity.Order;
-import jakarta.persistence.SequenceGenerators;
+import com.my.foody.domain.orderMenu.repo.dto.OrderProjectionDto;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
@@ -18,4 +19,19 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             "left join fetch o.address a " +
             "where o.id = :orderId")
     Optional<Order> findOrderWithDetails(@Param("orderId") Long orderId);
+
+
+    @Query("select new com.my.foody.domain.orderMenu.repo.dto.OrderProjectionDto(" +
+            "s.name, o.orderStatus, a.roadAddress, a.detailedAddress, o.totalAmount, " +
+            "GROUP_CONCAT(m.menuName SEPARATOR,', '), o.id, o.createdAt) " +
+            "from Order o " +
+            "join o.user u " +
+            "join o.store s " +
+            "join o.address a " +
+            "join o.orderMenus om " +
+            "join om.menu m " +
+            "where u.id = :userId " +
+            "group by o.id")
+    Page<OrderProjectionDto> findByUserId(@Param("userId") Long userId, Pageable pageable);
+
 }
