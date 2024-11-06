9eb0c1f01fd8262722f52d50403d76098f9a529e
soung90 <sounglikane@gmail.com>
2024-11-06 10:58:49 +0900

    feat(cart-repo): Custom Query Method 수정 #10

--- src/main/java/com/my/foody/domain/cart/repo/CartRepository.java
+++ src/main/java/com/my/foody/domain/cart/repo/CartRepository.java
@@ -14,6 +14,8 @@ import java.util.Optional;
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
   Page<Cart> findByUserId(Long userId, Pageable pageable);
+  @Query("SELECT c FROM Cart c WHERE c.id = :cartId AND c.user.id = :userId AND c.store.id = :storeId")
+  Optional<Cart> findWithStoreAndMenuByIdAndUserIdAndStoreId(@Param("cartId") Long cartId, @Param("userId") Long userId, @Param("storeId") Long storeId);
 
   Optional<Cart> findByIdAndUser(Long cartId, User user);
 
