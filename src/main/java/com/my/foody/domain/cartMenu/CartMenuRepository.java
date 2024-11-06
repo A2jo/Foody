5b74939afbdcf430123ef5fb5f4d675e896d8fa2
soung90 <sounglikane@gmail.com>
2024-11-06 10:25:08 +0900

    feat(order_menu-repo): method 추가 #10

--- src/main/java/com/my/foody/domain/cartMenu/CartMenuRepository.java
+++ src/main/java/com/my/foody/domain/cartMenu/CartMenuRepository.java
@@ -1,8 +1,12 @@
 package com.my.foody.domain.cartMenu;
 
+import com.my.foody.domain.cart.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartMenuRepository extends JpaRepository<CartMenu, Long> {
+    List<CartMenu> findByCart(Cart cart);
 }
