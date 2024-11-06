256d7fb1c88d9b8ad69286d287e0e3ca3465a7fd
JIMINJI1 <mj013031@gmail.com>
2024-11-06 22:14:46 +0900

    fix(menu-controller): 병합 후 충돌로 다시 파일 넣음 #66

--- src/main/java/com/my/foody/domain/menu/controller/MenuController.java
+++ src/main/java/com/my/foody/domain/menu/controller/MenuController.java
@@ -37,6 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 import com.my.foody.domain.menu.dto.req.MenuCreateReqDto;
 import com.my.foody.domain.menu.dto.resp.MenuCreateRespDto;
+import com.my.foody.domain.menu.dto.resp.MenuReadResponseDto;
 import com.my.foody.domain.menu.service.MenuService;
 import com.my.foody.global.config.valid.CurrentUser;
 import com.my.foody.global.config.valid.RequireAuth;
@@ -84,11 +85,26 @@ public class MenuController {
 
     private final MenuService menuService;
 
+    // 1. 메뉴등록
+    @PostMapping("/api/owners/stores/{storeId}/menus")
+    // 권한 확인
+    @RequireAuth(userType = UserType.OWNER)
+    public ResponseEntity<ApiResult<MenuCreateRespDto>> createMenu(@PathVariable("storeId") Long storeId,
+                                                                   @Valid @RequestBody MenuCreateReqDto menuCreateReqDto,
+                                                                   @CurrentUser TokenSubject tokenSubject) {
+        //userId
+        Long ownerId = tokenSubject.getId();
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResult.success(menuService.createMenu(storeId, menuCreateReqDto, ownerId)));
+    }
+
+
     // 메뉴조회
     @GetMapping("/api/owners/stores/{storeId}/menus")
     //권한 확인
     @RequireAuth(userType = UserType.OWNER)
-    public ResponseEntity<ApiResult<Page<MenuReadResponseDto>>> getMenus(@PathVariable("storeId") Long storeId,
+    public ResponseEntity<ApiResult<MenuReadResponseDto>> getMenus(@PathVariable("storeId") Long storeId,
                                                                          @CurrentUser TokenSubject tokenSubject,
                                                                          @RequestParam(defaultValue = "0") int page,
                                                                          @RequestParam(defaultValue = "10") int limit) {
@@ -96,7 +112,7 @@ public class MenuController {
         Long ownerId = tokenSubject.getId();
 
 
-        Page<MenuReadResponseDto> responseDto = menuService.getMenus(storeId, ownerId, page, limit);
+        MenuReadResponseDto responseDto = menuService.getMenus(storeId, ownerId, page, limit);
         return ResponseEntity.ok(ApiResult.success(responseDto));
     }
 
