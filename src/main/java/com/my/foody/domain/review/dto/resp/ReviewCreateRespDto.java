051b7897a73763d530c29d3431176172558c4411
JIMINJI1 <mj013031@gmail.com>
2024-11-06 12:58:30 +0900

    feat(review-dto): 리뷰작성 dto #74

--- src/main/java/com/my/foody/domain/review/dto/resp/ReviewCreateRespDto.java
+++ src/main/java/com/my/foody/domain/review/dto/resp/ReviewCreateRespDto.java
@@ -1,4 +1,19 @@
 package com.my.foody.domain.review.dto.resp;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class ReviewCreateRespDto {
+    private static final String SUCCESS_MESSAGE = "등록 완료 되었습니다";
+    private final String message;
+
+    public ReviewCreateRespDto() {
+        this.message = SUCCESS_MESSAGE;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
